aa450e4ae053f94b8730c0381e22a9647f897d2f
Victor Bilouro <bilouro@bilouro.com>
2021-01-05 00:15:21 -0300

    adding unique mapping for all the file.

--- index.js
+++ index.js
@@ -1,114 +1,39 @@
 const fs = require('fs');
 const os = require('os');
+const { exit } = require('process');
+const { getObjectsFromFile } = require("./positinalFleHelper");
 
 fs.readFile('./files/m80', 'utf8', (err, data) => {
-    const dataObjects = getObjectsAndEventsFromFile(data);
-    console.log(">>>> data objects <<<")
+    const dataObjects = getObjectsFromFile(data, getFileMapping());
     console.log(dataObjects);
-
-    // const adjustmentList = dataObjects.filter(obj => obj.eventType == 'A');
-    // const blockedList    = dataObjects.filter(obj => obj.eventType == 'B');
-    // const unblockedlist  = dataObjects.filter(obj => obj.eventType == 'U');
-
-    // const stockAdjustmentHasBeenDoneEvents = createStockAdjustmentHasBeenDoneEvents(adjustmentList);
-    // const productHasBeenBlockedEvents = createProductHasBeenBlockedEvents(blockedList);
-    // const productHasBeenUnblockedEvents = createProductHasBeenUnblockedEvents(unblockedlist);
-    // console.log(">>>> Adjustments events <<<")
-    // console.log(stockAdjustmentHasBeenDoneEvents);
-
-    //Perform Insert
 });
 
-function getObjectsAndEventsFromFile(data) {
-    const dataObjectsArray = [];  
-    const eventsArray = [];  
-    const lines = data.split(os.EOL);
-    let eventDate = 0;
-
-    let dataLineReceivedCount = 0;
-    for (i = 0; i < lines.length; i++) {
-        let line = lines[i];
-        switch (line.substr(0, 2)) {
-            case '00':
-                //0020201110
-                eventDate = processLine00(line);
-                break;
-            case '01':
-            case '02':
-            case '03':
-                //01123123B40
-                dataObjectsArray.push( processLine01(line, eventDate) );
-                dataLineReceivedCount++;
-                break;
-            case '99':
-                //990000003
-                processLine99(line, dataLineReceivedCount);
-                break;
-            default:
-                throw `lineType not specified in documentation ${line.substr(0, 2)}`;
-        }
-
-    }
-    return dataObjectsArray;
-}
-function processLine99(line, dataLineReceivedCount) {
-    const dataLinesSentCount = parseInt(line.substring(2, 9));
-    if (dataLinesSentCount != dataLineReceivedCount) {
-        console.log(`quantidade errada de itens. Contados ${dataLineReceivedCount} informados no rodapé ${dataLinesSentCount}`);
-        throw 'quantidade errada';
-    }
-}
-
-function processLine01(line, eventDate)  {
-    const lineMapping = [
-        {name: 'productNo',  initial_position:  2, length:  6, type: 'integer' },
-        {name: 'eventType',  initial_position:  8, length:  1, type: 'string'  },
-        {name: 'eventCode',  initial_position:  9, length:  2, type: 'string'  },
-        {name: 'eventNumber',  fixedValue: '99', type: 'fixed'  },
-        {name: 'eventClass',  fixedValue: 'AAA', type: 'fixed'  },
-    ];
-
-    let dataObject = getDataFromFileLineByMapping(lineMapping, line);
-    dataObject.eventDate = eventDate;
-
-    return dataObject;
-}
-
-function getDataFromFileLineByMapping(lineMapping, line) {
-    let dataObject = {};
-    lineMapping.map((attribute) => {
-
-        switch (attribute.type) {
-            case 'fixed':
-                dataObject[attribute.name] = attribute.fixedValue;
-                break;
-            default:
-                dataObject[attribute.name] =  line.substring(attribute.initial_position, attribute.initial_position + attribute.length)
-                break;
-        }
-           
-    });
-    return dataObject;
-}
-
-function processLine00(line) {
-    const year = line.substring(2, 6);
-    const month = line.substring(6, 8);
-    const day = line.substring(8, 10);
-    return new Date(year, month, day);
-}
-
-
-function createStockAdjustmentHasBeenDoneEvents(adjustmentList) {
-    const stockAdjustmentHasBeenDoneEvents = [];
-    adjustmentList.map( adjustment => {
-
-        stockAdjustmentHasBeenDoneEvents.push({
-            nun_bu: adjustment.nunbu,
-            cod_typett: 2,
-            nun_ett:adjustment.codact,
-            cod_typmvt:adjustment.codmvt,
-        })
-    })
-    return stockAdjustmentHasBeenDoneEvents;
-}
+/**
+ *  Insert a empty list of attributes to ignore a line of file: 
+ *                ["00", []], 
+ */
+function getFileMapping() {
+    return {
+        discriminatorInitialPostion : 0,
+        discriminatorLenght : 2,
+        lines : new Map(
+            [
+                ["00", [
+                    { name: 'codExc', initialPosition: 0, length: 2, type: 'string', required: true },
+                    { name: 'eventDate', initialPosition: 2, length: 8, type: 'date', required: true, dateFormat: 'YYYYMMDD' },
+                ]], 
+                ["01", [  
+                    { name: 'codExc', initialPosition: 0, length: 2, type: 'string', required: true },
+                    { name: 'productNo', initialPosition: 2,  length: 6,  type: 'integer', required: true },
+                    { name: 'eventType', initialPosition: 8,  length: 1,  type: 'string', required: true },
+                    { name: 'eventCode', initialPosition: 9,  length: 2,  type: 'integer', required: false },
+                    { name: 'eventDesc', initialPosition: 11, length: 42, type: 'string', required: false },
+                ]], 
+                ["99", [  
+                    { name: 'codExc', initialPosition: 0, length: 2, type: 'string', required: true },
+                    { name: 'linesSentCount', initialPosition: 2, length: 7, type: 'integer', required: true },
+                ]], 
+            ]            
+        )
+    };
+};
---
+++ positinalFleHelper.js
@@ -0,0 +1,102 @@
+const os = require('os');
+const moment = require('moment');
+
+function getObjectsFromFile(data, fileMapping) {
+    let dataObjectsArray = [];
+
+    const lines = data.split(os.EOL);
+    checkFileIsValid(lines);
+
+    dataObjectsArray = lines.map((line, lineNumber) => {
+        let discriminator = line.substr(fileMapping.discriminatorInitialPostion, fileMapping.discriminatorLenght);
+        checkDiscriminatorIsValid(discriminator, fileMapping, lineNumber);
+
+        let lineMapping = fileMapping.lines.get(discriminator);
+        if (checkLineMappingIsValid(lineMapping, discriminator, lineNumber))
+            return getObjectFromLine(lineMapping, line, lineNumber);
+        
+    });
+    return dataObjectsArray;
+}
+
+function getObjectFromLine(lineMapping, line, lineNumber) {
+    const dataObject = {};
+    for (i=0;i<lineMapping.length;i++){
+        const attribute = lineMapping[i];
+        checkMappingIsValid(attribute);
+
+        const value = line.substring(attribute.initialPosition, attribute.initialPosition + attribute.length);
+        checkRequiredIsValid(attribute, value, lineNumber);
+        switch (attribute.type) {
+            case 'integer':
+                dataObject[attribute.name] = parseInteger(value, attribute, lineNumber);
+                break;
+            case 'date':
+                dataObject[attribute.name] = parseDate(value, attribute, lineNumber);
+                break;
+            case 'string':
+                dataObject[attribute.name] =  value;
+                break;
+            default:
+                throw `Attribute type "${attribute.type}", declared in lineMapping is not valid or not implemented. line number ${lineNumber}.`;
+        }
+    }
+    return dataObject;
+}
+
+function parseDate(value, attribute, lineNumber) {
+    if (!attribute.dateFormat)
+        throw `The attribute ${attribute.name} has no dateFormat declared in lineMapping`;
+
+    const dateMoment = moment(value, attribute.dateFormat);
+    if (!dateMoment.isValid())
+        throw `Value "${value}", is not a valid Date for the attribute ${attribute.name} (${attribute.dateFormat}). line number ${lineNumber}.`;
+    return dateMoment.toDate();
+}
+
+function parseInteger(value, attribute, lineNumber) {
+    if (value && isNaN(value))
+        throw `Value "${value}", is not a valid Number for the attribute ${attribute.name}. line number ${lineNumber}.`;
+
+    const convertedValue = parseInt(value);
+    if (isNaN(convertedValue))
+        return null;
+    
+    return convertedValue;
+}
+
+function checkFileIsValid(lines) {
+    if (!lines || (lines.length == 1 && lines[0] == ''))
+        throw `file has no lines`;
+}
+
+function checkLineMappingIsValid(lineMapping, discriminator, lineNumber) {
+    if (!lineMapping || lineMapping.length == 0) {
+        console.log(`missing lineMapping for discriminator "${discriminator}". Line #${lineNumber} IGNORED`);
+        return false;
+    }
+    return true;
+}
+
+function checkDiscriminatorIsValid(discriminator, fileMapping, lineNumber) {
+    if (!discriminator)
+        throw `could not get discriminator at position ${fileMapping.discriminatorInitialPostion}(${fileMapping.discriminatorLenght}). line number ${lineNumber}.`;
+}
+
+function checkRequiredIsValid(attribute, value, lineNumber) {
+    if (attribute.required && !value)
+        throw `${attribute.name} is required but has no value. line number ${lineNumber}.`;
+}
+
+function checkMappingIsValid(attribute) {
+    if (!attribute)
+        throw 'missing mapping attribute';
+    if (!attribute.name)
+        throw 'missing mapping attribute: name';
+    if (!attribute.initialPosition && attribute.initialPosition != 0) //ZERO is valid
+        throw 'missing mapping attribute: initialPosition';
+    if (!attribute.length)
+        throw 'missing mapping attribute: length';
+}
+
+exports.getObjectsFromFile = getObjectsFromFile;
