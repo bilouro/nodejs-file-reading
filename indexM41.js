530cf5a5df41fb411f283577d1534ae55f0b4774
NunoLopes-LM <nuno.c.lopes@ext.leroymerlin.pt>
2021-01-13 18:02:56 +0000

    WIP M41 event creation

--- indexM41.js
+++ indexM41.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const os = require('os');
 const { exit } = require('process');
 const { getObjectsFromFile } = require("./positionalFileHelper");
+const { Converter } = require("./convertHelper");
 
 fs.readFile('./files/358M4121010500151789.txt', 'utf8', (err, data) => {
     const dataObjects = getObjectsFromFile(data, getFileMapping());
-    console.log(dataObjects);
+    // console.log(JSON.stringify(dataObjects, null, 2));
+    const eventObjects = new Converter().convert(dataObjects, getBindingMap());
+    console.log(JSON.stringify(eventObjects, null, 2));
 });
 
 /**
@@ -20,6 +23,7 @@ function getFileMapping() {
       [
         '00.00',
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: true },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: true },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: true },
@@ -40,7 +44,7 @@ function getFileMapping() {
           { name: 'pardst', initialPosition: 138, length: 30, type: 'string', required: false },
           { name: 'codact', initialPosition: 168, length: 3, type: 'string', required: false },
           { name: 'iglsit', initialPosition: 171, length: 3, type: 'integer', required: false },
-          { name: 'edisit', initialPosition: 174, length: 14, type: 'string', required: false },
+          { name: 'edisit', initialPosition: 174, length: 14, type: 'integer', required: false },
           { name: 'imaexc', initialPosition: 188, length: 8, type: 'integer', required: false },
           { name: 'idemsg', initialPosition: 196, length: 30, type: 'string', required: false },
           { name: 'dsiexc', initialPosition: 226, length: 30, type: 'string', required: false },
@@ -49,6 +53,7 @@ function getFileMapping() {
       [
         '41.00', 
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: false },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: false },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: false },
@@ -61,8 +66,8 @@ function getFileMapping() {
           { name: 'codldr', initialPosition: 57, length: 3, type: 'string', required: false },
           { name: 'codtre', initialPosition: 60, length: 3, type: 'string', required: false },
           { name: 'oricde', initialPosition: 63, length: 1, type: 'string', required: false },
-          { name: 'codact', initialPosition: 64, length: 3, type: 'string', required: false },
-          { name: 'codfou', initialPosition: 67, length: 14, type: 'string', required: false },
+          { name: 'codact', initialPosition: 64, length: 3, type: 'integer', required: false },
+          { name: 'codfou', initialPosition: 67, length: 14, type: 'integer', required: false },
           { name: 'codtra', initialPosition: 81, length: 14, type: 'string', required: false },
           { name: 'dtirec', initialPosition: 95, length: 12, type: 'date', required: false, dateFormat: 'YYYYMMDDhhmm' },
           { name: 'heirec', initialPosition: 103, length: 4, type: 'integer', required: false, dateFormat: 'hhmm' },
@@ -92,6 +97,7 @@ function getFileMapping() {
       [
         '41.01',
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: false },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: false },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: false },
@@ -114,6 +120,7 @@ function getFileMapping() {
       [
         '41.20',
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: false },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: false },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: false },
@@ -156,6 +163,7 @@ function getFileMapping() {
       [
         '41.30',
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: false },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: false },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: false },
@@ -194,6 +202,7 @@ function getFileMapping() {
       [
         '41.80',
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: false },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: false },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: false },
@@ -218,6 +227,7 @@ function getFileMapping() {
       [
         '41.99',
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: false },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: false },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: false },
@@ -236,6 +246,7 @@ function getFileMapping() {
       [
         '99.00',
         [
+          { name: 'regexc', initialPosition: 0, length: 5, type: 'string', required: true },
           { name: 'codexc', initialPosition: 0, length: 2, type: 'integer', required: true },
           { name: 'sepexc', initialPosition: 2, length: 1, type: 'string', required: true },
           { name: 'scoexc', initialPosition: 3, length: 2, type: 'integer', required: true },
@@ -255,4 +266,125 @@ function getFileMapping() {
       ],
     ]),
   };
+};
+
+function getBindingMap() {
+  return  {
+      header: 0,   //null for no header
+      footer: -1,  //null for no footer
+      skipObjectIf: new Map([
+        ['regexc', [ '41.01', '41.20', '41.30', '41.80', '41.99' ],],
+      ]),
+      bindings: [
+          { destination: 'identifier', source: 'numrec',                  type: 'copy' },
+          { destination: 'supplyCircuitType',                             type: 'fixed', value: 'stock' },
+          { destination: 'validationDate',                                type: 'function', value: bind__date_epoch },
+          { destination: 'startDate',                                     type: 'function', value: bind__date_epoch },
+          { destination: 'closedDate',                                    type: 'function', value: bind__date_epoch },
+          { destination: 'deliveryNoteNumber',                            type: 'fixed', value: null },
+          { destination: 'shipmentIdentifier',                            type: 'fixed', value: null },
+          { destination: 'expectedPackages',                              type: 'function', value: bind__expectedPackages },
+          { destination: 'packages',                                      type: 'function', value: bind__packages },
+          { destination: 'deliveryLocation.businessUnitIdentifier',       type: 'function', value: bind__businessUnitIdentifier },
+          { destination: 'deliveryLocation.identifier', source: 'codact', type: 'copy' },
+          { destination: 'deliveryLocation.type',                         type: 'fixed', value: 'warehouse' },
+          { destination: 'shippingLocation.businessUnitIdentifier',       type: 'function', value: bind__businessUnitIdentifier },
+          { destination: 'shippingLocation.identifier', source: 'codfou', type: 'copy' },
+          { destination: 'shippingLocation.type',                         type: 'fixed', value: 'supplier' },
+          { destination: 'receptionLines',                                type: 'function', value: bind__receptionLines },
+        ]
+  }            
+};
+
+function bind__date_epoch(currentObject, header, footer, forthcomingObjectList) {
+  return currentObject.dtrrec.getTime();
+};
+
+function getExpectedPackagesBindingMap() {
+  return {
+    bindings: [
+      { destination: 'quantity', type: 'fixed', value: null },
+      { destination: 'type',     type: 'fixed', value: null },
+    ]
+  }
+};
+
+function bind__expectedPackages(currentObject) {
+  return new Converter().convert([currentObject], getExpectedPackagesBindingMap())
+};
+
+function getPackagesBindingMap() {
+  return {
+    bindings: [
+      { destination: 'quantity', type: 'function', value: bind__packages_quantity },
+      { destination: 'type',     type: 'fixed', value: 'parcel' },
+    ]
+  }
+};
+
+function bind__packages(currentObject, header, footer, forthcomingObjectList) {
+  // creating array of 41.20 lines with children array of 41.30 lines
+  const packagesArray = [];
+  for (let i = 0; i < forthcomingObjectList.length; i++) {
+    if (forthcomingObjectList[i].codexc === 41 && forthcomingObjectList[i].scoexc === 20) {
+      packagesArray.push({...forthcomingObjectList[i]});
+    } else if (forthcomingObjectList[i].codexc === 41 && forthcomingObjectList[i].scoexc === 30) {
+      const lastPushedParentObject = packagesArray[packagesArray.length - 1];
+      if (!lastPushedParentObject.children) lastPushedParentObject.children = [];
+      lastPushedParentObject.children.push({...forthcomingObjectList[i]});
+    } else break;
+  }
+
+  return new Converter().convert(packagesArray, getPackagesBindingMap());
+};
+
+function bind__packages_quantity(currentObject) { 
+  const firstChild = currentObject.children[0];
+  if (firstChild.pcbpro !== 0) {
+    if (firstChild.spcpro !== 0) {
+      return (currentObject.uvcrec + currentObject.uvcimm) / firstChild.pcbpro / firstChild.spcpro;
+    }
+    return (currentObject.uvcrec + currentObject.uvcimm) / firstChild.pcbpro;
+  }
+  return currentObject.uvcrec + currentObject.uvcimm;
+};
+
+function bind__businessUnitIdentifier(currentObject, header, footer, forthcomingObjectList) {
+  return header.edisit;
+};
+
+function getReceptionLinesMap() {
+  return {
+    bindings: [
+      { destination: 'identifier', source: 'nlirec',          type: 'copy' },
+      { destination: 'productReferenceBU', source: 'codpro',  type: 'copy' },
+      { destination: 'productReferenceAdeo',                  type: 'fixed', value: null },
+      { destination: 'quantity', source: 'uvcrec',            type: 'copy' },
+      { destination: 'deliveryNoteQuantity',                  type: 'fixed', value: null },
+      { destination: 'differenceQuantity',                    type: 'fixed', value: 0 },
+      { destination: 'physicalStockExpirationDate',           type: 'function', value: bind__receptionLines_physicalStockExpirationDate },
+      { destination: 'transferID',                            type: 'fixed', value: null },
+      { destination: 'purchaseOrderID', source: 'refrec',     type: 'copy' },
+      { destination: 'customerOrderNumber',                   type: 'fixed', value: null },
+    ]
+  }
+};
+
+function bind__receptionLines(currentObject, header, footer, forthcomingObjectList) {
+  const receptionLinesArray = [];
+  for (let i = 0; i < forthcomingObjectList.length; i++) {
+    if (forthcomingObjectList[i].codexc === 41 && forthcomingObjectList[i].scoexc === 20) {
+      receptionLinesArray.push(forthcomingObjectList[i]);
+    } else if (forthcomingObjectList[i].codexc === 41 && forthcomingObjectList[i].scoexc === 00) {
+      break;
+    }
+  }
+  // console.log(JSON.stringify(receptionLinesArray, null, 2));
+
+  return new Converter().convert(receptionLinesArray, getReceptionLinesMap());
+};
+
+function bind__receptionLines_physicalStockExpirationDate(currentObject, header, footer, forthcomingObjectList) {
+  if (currentObject.datfvi) return currentObject.datfvi.getTime();
+  return null;
 }
