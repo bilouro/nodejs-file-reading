f9cf0b3947dd68ee4b15036afd80b339e9b7a86e
NunoLopes-LM <nuno.c.lopes@ext.leroymerlin.pt>
2021-01-22 09:51:28 +0000

    changed the unification unique value

--- indexM51.js
+++ indexM51.js
@@ -7,26 +7,42 @@ const { getFileMapping } = require("./mappers/m51FileMapping");
 const { uniquifyList } = require("./uniquifyHelper");
 
 function get5100UniqueValue(obj){
-  return obj.cliliv;
+  let found = false;
+  let foundValue = null;
+  obj.children5105.forEach(({valrub1, valrub2, valrub3, valrub4, codrub1, codrub2, codrub3, codrub4}) => {
+    if (!found) {
+      [codrub1, codrub2, codrub3, codrub4].forEach((codrub, index) => {
+        if (codrub === 'CIRL') {
+          foundValue = [valrub1, valrub2, valrub3, valrub4][index];
+          found = true;
+        };
+      });
+    }
+  })
+
+  return foundValue ? obj.cliliv + foundValue : obj.cliliv;
 }
 
-fs.readFile('./files/358M5121010700153307.txt', 'utf8', (err, data) => {
-    const dataObjects = getObjectsFromFile(data, getFileMapping()); //salvar esta lista no banco.
-    console.log('======================================================');
-    console.log('=         DATA OBJECTS                               =');
-    console.log('======================================================');
-    console.log(dataObjects);
+fs.readFile('./files/358M5121010700153317.txt', 'utf8', (err, data) => {
+    const dataObjects = getObjectsFromFile(data, getFileMapping());
+    // console.log('======================================================');
+    // console.log('=         DATA OBJECTS                               =');
+    // console.log('======================================================');
+    // console.log(dataObjects);
 
-    const list5100 = dataObjects.filter(obj => obj.codexc == '51' && obj.scoexc == 0); //nao salvar esta lista no banco
-    console.log('======================================================');
-    console.log('=         FILTER 51.00                               =');
-    console.log('======================================================');
-    console.log(list5100);
+    // filter by 51.00 lines
+    const list5100 = dataObjects.filter(obj => obj.codexc == '51' && obj.scoexc == 0);
 
-    const uniqueList = uniquifyList(list5100, ['children5101', 'children5120', 'children5199'], get5100UniqueValue);  //processar essa lista no converter.
-    console.log('======================================================');
-    console.log('=         DISTINCT                                   =');
-    console.log('======================================================');
+
+    const uniqueList = uniquifyList(list5100, ['children5101', 'children5105', 'children5130', 'children5199'], get5100UniqueValue);
+    // console.log('======================================================');
+    // console.log('=         DISTINCT                                   =');
+    // console.log('======================================================');
+    // console.log(uniqueList);
+
+    // add header and footer to new list
+    uniqueList.unshift(dataObjects[0]);
+    uniqueList.push(dataObjects[dataObjects.length - 1]);
     console.log(uniqueList);
     
 });
