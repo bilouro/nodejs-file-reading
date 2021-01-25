15c59234027428e29bc9ca056c7738d97aea6d15
NunoLopes-LM <nuno.c.lopes@ext.leroymerlin.pt>
2021-01-25 17:46:57 +0000

    writing the m90 result into an output file

--- indexM90.js
+++ indexM90.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const os = require('os');
+const moment = require('moment');
 const { exit } = require('process');
 const { getObjectsFromFile } = require("./positionalFileHelper");
 const { Converter } = require("./convertHelper");
@@ -10,5 +11,23 @@ fs.readFile('./files/358M9020122900148760.txt', 'utf8', (err, data) => {
     const dataObjects = getObjectsFromFile(data, getFileMapping());
     // console.log(dataObjects);
     const eventObjects = new Converter().convert(dataObjects, [getBindingMap()], { header: 0, footer: -1});
+
+    const timestamp = moment().format('YYYYMMDDhhmmssSSSSS');
+    const fileName = `./files/m90-output-${timestamp}.txt`;
+    fs.open(fileName, 'a', (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    )
+    const writer = fs.createWriteStream(fileName, { flags: 'a' });
+    eventObjects.forEach(async obj => {
+      writer.write(JSON.stringify(obj) + '\n', (err) => {
+          if (err) {
+            console.log(err);
+          }
+        }
+      )
+    })
     console.log(JSON.stringify(eventObjects, null, 2));
 });
